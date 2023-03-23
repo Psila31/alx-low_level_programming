439c68e59269cd0404b0beca61d8f4301ea65de9
Psila31 <patricksila31@gmail.com>
2023-03-23 02:26:00 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0 to 9
+ * @c: char to be checked
+ * return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for(a = 48; a <= 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	
+	return (0);
+}
