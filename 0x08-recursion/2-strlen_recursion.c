048d8a36887ad2e5379ae6669d8e3091ffe79476
Psila31 <patricksila31@gmail.com>
2023-04-06 03:30:24 -0700

    fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,12 @@
 */
 int _strlen_recursion(char *s)
 {
-	int legnth = 0;
+	int n = 0;
 
 	if (*s)
 	{
-		legnth++;
-		legnth += strlen_recursion(s + 1);
+		n++;
+		n += _strlen_recursion(s + 1);
 	}
-	return (legnth);
+	return (n);
 }
