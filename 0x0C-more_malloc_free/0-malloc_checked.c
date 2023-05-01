bd07c741b42feec4dca6c09d871cc872a817b915
Psila31 <patricksila31@gmail.com>
2023-05-01 06:23:03 -0700

    initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - uses malloc
+ * @b: number of bytes 
+ *
+ * Return: a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
